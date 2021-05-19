902d396bffa0a3bdf1606779c07fa592de795ffc
gyuuuu <kai9702@gmail.com>
2021-05-19 16:23:48 +0900

    add express.json

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const PORT = 5000;
 
 const app = express();
+
+app.use(express.json);
 app.get('/', (req, res)=>{
-  res.send('Hello World');
+    res.send('Hello World');
 });
 
 app.listen(PORT);
